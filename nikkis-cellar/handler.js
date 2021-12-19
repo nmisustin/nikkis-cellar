4064badb41203dde7edce61d763d410f7e62bd76
nmisustin <nmisustin@gmail.com>
2021-12-19 00:03:16 -0500

    added serverless and have get by id and post functions

---
+++ nikkis-cellar/handler.js
@@ -0,0 +1,72 @@
+const AWS = require("aws-sdk");
+const express = require("express");
+const serverless = require("serverless-http");
+
+const app = express();
+
+const CELLAR_TABLE = process.env.CELLAR_TABLE;
+const dynamoDbClient = new AWS.DynamoDB.DocumentClient();
+
+app.use(express.json());
+
+app.get("/cellar/:id", async function (req, res) {
+  const params = {
+    TableName: CELLAR_TABLE,
+    Key: {
+      id: req.params.id,
+    },
+  };
+
+  try {
+    const { Item } = await dynamoDbClient.get(params).promise();
+    if (Item) {
+      const { id, vintage, winery, subblock, varietal, quantity, rating, notes } = Item;
+      res.json({ id, vintage, winery, subblock, varietal, quantity, rating, notes });
+    } else {
+      res
+        .status(404)
+        .json({ error: 'Could not find bottle with provided "id"' });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Could not retreive bottle" });
+  }
+});
+
+app.post("/cellar", async function (req, res) {
+  const { id, vintage, winery, subblock, varietal, quantity, rating, notes } = req.body;
+  if (typeof id !== "string") {
+    res.status(400).json({ error: '"id" must be a string' });
+  } 
+
+  const params = {
+    TableName: CELLAR_TABLE,
+    Item: {
+        id : id,
+        vintage  : vintage,
+        winery   : winery,
+        subblock : subblock,
+        varietal : varietal,
+        quantity : quantity,
+        rating  : rating,
+        notes : notes
+    },
+  };
+
+  try {
+    await dynamoDbClient.put(params).promise();
+    res.json({ id, vintage, winery, subblock, varietal, quantity, rating, notes  });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Could not create bottle" });
+  }
+});
+
+app.use((req, res, next) => {
+  return res.status(404).json({
+    error: "Not Found",
+  });
+});
+
+
+module.exports.handler = serverless(app);
