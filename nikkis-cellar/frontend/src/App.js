433507af5f9542771d0c32ddbd12eac950d53cbb
nmisustin <nmisustin@gmail.com>
2022-02-06 21:18:14 -0500

    added react and installed material dependencies

---
+++ nikkis-cellar/frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import BasicTable from './components/Table';
+
+function App() {
+  return (
+    <div className="App">
+      <h3>Nikki's Cellar</h3>
+      <BasicTable></BasicTable>
+    </div>
+  );
+}
+
+export default App;
